f733bcb0b34e683f16f17a92f20ee09863906317
Kabez01 <kobzuu@gmail.com>
2020-02-12 18:34:21 +0100

    Updates

---
+++ react_Simple_Contacts_Editor/precache-manifest.c2a92a0101746fb71bfcc221a40aee7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee74f0d203f67b45c5a48dd7c4feaade",
+    "url": "/MyRepo/react_Simple_Contacts_Editor/index.html"
+  },
+  {
+    "revision": "59653fa0a5108b1484dd",
+    "url": "/MyRepo/react_Simple_Contacts_Editor/static/css/main.841f58e0.chunk.css"
+  },
+  {
+    "revision": "2477ed8a260a81aebad7",
+    "url": "/MyRepo/react_Simple_Contacts_Editor/static/js/2.ff643e6f.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/MyRepo/react_Simple_Contacts_Editor/static/js/2.ff643e6f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59653fa0a5108b1484dd",
+    "url": "/MyRepo/react_Simple_Contacts_Editor/static/js/main.843a59c3.chunk.js"
+  },
+  {
+    "revision": "01f9c624cce663aa1911",
+    "url": "/MyRepo/react_Simple_Contacts_Editor/static/js/runtime-main.ca480239.js"
+  }
+]);
