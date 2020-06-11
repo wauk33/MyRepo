d68224d1d3c3e2f33faa42830c48835d5a7fd5ae
Kabez01 <kobzuu@gmail.com>
2020-06-11 20:32:57 +0200

    Kurort website upload

--- kurort/js/main.js
+++ kurort/js/main.js
@@ -1,15 +1,36 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 const brand = document.getElementById("nav-brand-id");
+const navbar = document.getElementById("navbar");
+
+$(document).click(function (event) {
+    let clickover = $(event.target);
+    let _opened = $(".navbar-toggler").hasClass("collapsed");
+    if (_opened === false && !clickover.hasClass("navbar-toggler")) {
+        $(".navbar-toggler").click();
+    }
+});
+
+$(document).scroll(function (event) {
+    let clickover = $(event.target);
+    let _opened = $(".navbar-toggler").hasClass("collapsed");
+    if (_opened === false && !clickover.hasClass("navbar-toggler")) {
+        $(".navbar-toggler").click();
+    }
+});
+
 if (window.scrollY > 140){
     brand.classList.add("nav-brand-hide")
+    navbar.classList.add("nav-transparent")
 }
 window.addEventListener("scroll", () => {
     if (window.scrollY > 140){
         brand.classList.add("nav-brand-hide")
+        navbar.classList.add("nav-transparent")
     }
     else{
         brand.classList.remove("nav-brand-hide")
+        navbar.classList.add("nav-transparent")
     }
 })
 
