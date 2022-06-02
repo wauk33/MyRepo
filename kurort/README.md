9dff065f51570b2db39399311963a4089c575842
wauk33 <kobzuu@gmail.com>
2022-06-02 16:57:39 +0200

    Update files structure

---
+++ kurort/README.md
@@ -0,0 +1,16 @@
+# Sea resort landing page
+> A simple landing page created with Bootstrap, SASS, HTML and JavaScript with libraries.
+
+## General info
+The site was made from a scratch by me with my own design.
+
+## Preview
+https://wauk33.github.io/LP_SeaResort
+> Hosted on GitHub Pages
+
+## Features
+* Responsive for vertical mobile
+
+To-do list:
+* Repair issues with not correct displaying background images on mobile devices
+* Optimize and improve performance
--- kurort/js/main.js
+++ kurort/js/main.js
@@ -1,23 +1,45 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-const brand = document.getElementById("nav-brand-id");
-if (window.scrollY > 140){
-    brand.classList.add("nav-brand-hide")
-}
-window.addEventListener("scroll", () => {
+    const brand = document.getElementById("nav-brand-id");
+    const navbar = document.getElementById("navbar");
+    
+    $(document).click(function (event) {
+        let clickover = $(event.target);
+        let _opened = $(".navbar-toggler").hasClass("collapsed");
+        if (_opened === false && !clickover.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+    
+    $(document).scroll(function (event) {
+        let clickover = $(event.target);
+        let _opened = $(".navbar-toggler").hasClass("collapsed");
+        if (_opened === false && !clickover.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+    
     if (window.scrollY > 140){
         brand.classList.add("nav-brand-hide")
+        navbar.classList.add("nav-transparent")
     }
-    else{
-        brand.classList.remove("nav-brand-hide")
-    }
-})
-
-var scene = document.getElementById('scene');
-var parallaxInstance = new Parallax(scene);    
-var scene2 = document.getElementById('scene2');
-var parallaxInstance = new Parallax(scene);        
-
-
-});
-
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 140){
+            brand.classList.add("nav-brand-hide")
+            navbar.classList.add("nav-transparent")
+        }
+        else{
+            brand.classList.remove("nav-brand-hide")
+            navbar.classList.add("nav-transparent")
+        }
+    })
+    
+    var scene = document.getElementById('scene');
+    var parallaxInstance = new Parallax(scene);    
+    var scene2 = document.getElementById('scene2');
+    var parallaxInstance = new Parallax(scene);        
+    
+    
+    });
+    
+    
